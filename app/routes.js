f03804b420153ec40b99649436cd0ecd03ff4c02
Jared <jaredcormier@utexas.edu>
2017-11-05 04:15:56 -0600

    initial commit

---
+++ app/routes.js
@@ -0,0 +1,26 @@
+// require express
+var express = require('express');
+var path    = require('path');
+
+// create our router object
+var router = express.Router();
+
+// export our router to use in server.js
+module.exports = router;
+
+// route for our homepage
+router.get('/', function(req, res){
+	res.render('pages/index');
+});
+
+// route for our about page
+router.get('/about', function(req, res){
+		res.render('pages/about');
+});
+
+router.get('/contact', function(req, res){
+		res.render('pages/contact');
+});
+router.post('/contact', function(req, res){
+		
+});
